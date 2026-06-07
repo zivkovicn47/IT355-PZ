e7a6491c5b09a9d01d0161f5f4f53df682e0c628
Nikola Zivkovic <nikola.d.zivkovic@gmail.com>
2026-06-07 18:34:25 +0200

    Inicijalni commit: Sprint Boot projektna struktura

---
+++ src/main/java/com/metropolitan/it355pz/It355pzApplication.java
@@ -0,0 +1,13 @@
+package com.metropolitan.it355pz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355pzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It355pzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metropolitan/it355pz/It355pzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.it355pz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355pzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
